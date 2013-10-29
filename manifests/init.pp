2e49e63127b1d67c6450ebc34ad631eedce99379
Will Farrington <wfarr@github.com>
2013-10-29 11:39:39 -0700

    QT is known broken on Mavericks

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,12 +8,24 @@ class qt {
   include homebrew
   include xquartz
 
-  homebrew::formula { 'qt':
-    before => Package['boxen/brews/qt'],
-  }
+  case $::macosx_productversion_major {
+    '10.9': {
+      warning('QT is currently unsupported by OS X Mavericks, not actually installing it!')
 
-  package { 'boxen/brews/qt':
-    ensure  => '4.8.5-boxen1',
-    require => Class['xquartz'],
+      Package <| title == 'boxen/brews/qt' |> {
+        ensure => absent,
+      }
+    }
+
+    default: {
+      homebrew::formula { 'qt':
+        before => Package['boxen/brews/qt'],
+      }
+
+      package { 'boxen/brews/qt':
+        ensure  => '4.8.5-boxen1',
+        require => Class['xquartz'],
+      }
+    }
   }
 }
--- spec/classes/qt_spec.rb
+++ spec/classes/qt_spec.rb
@@ -1,18 +1,30 @@
 require 'spec_helper'
 
 describe 'qt' do
-  let(:facts) do
+  let(:default_facts) do
     {
       :boxen_home => '/opt/boxen',
       :boxen_user => 'testuser',
     }
   end
 
-  it do
-    should include_class('homebrew')
-    should include_class('xquartz')
+  context "10.8" do
+    let(:facts) { default_facts.merge(:macosx_productversion_major => '10.8')}
 
-    should contain_homebrew__formula('qt')
-    should contain_package('boxen/brews/qt').with_ensure('4.8.5-boxen1')
+    it do
+      should include_class('homebrew')
+      should include_class('xquartz')
+
+      should contain_homebrew__formula('qt')
+      should contain_package('boxen/brews/qt').with_ensure('4.8.5-boxen1')
+    end
+  end
+
+  context "10.9" do
+    let(:facts) { default_facts.merge(:macosx_productversion_major => '10.9')}
+
+    it do
+      should_not contain_package('boxen/brews/qt').with_ensure('4.8.5-boxen1')
+    end
   end
 end
